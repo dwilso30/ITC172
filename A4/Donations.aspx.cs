1d7f7f73c4398eda1ded37c2e1cad4cb4e954738
dwilso30 <dwilso30@seattlecentral.edu>
2017-02-15 10:38:51 -0800

    Add files via upload

---
+++ A4/Donations.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Donations : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["userKey"] == null)
+            Response.Redirect("Default.aspx");
+
+        Community_AssistEntites cae = new Community_AssistEntites();
+        int key = (int)Session["userKey"];
+        var dons = from d in cae.Donations
+                   where d.PersonKey == key;
+                   select d;
+        GridView1.DataSource = dons.ToList();
+        GridView1.DataBind();
+    }
+}
---
+++ A4/Register.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SumbitButton_Click(object sender, EventArgs e)
+    {
+        SubmitRegistration();
+    }
+
+    protected void SubmitRegistration()
+    {
+        string lastname = LastNameTextBox.Text;
+        string fristname = FirstNameTextBox.Text;
+        string email = EmailTextBox.Text;
+        string apratment = ApartmentTextBox.Text;
+        string street = StreetTextBox.Text;
+        string city = CityTextBox.Text;
+        string state = StateTextBox.Text;
+        string zip = ZipcodeTextBox.Text;
+        string home = HomeTextBox.Text;
+        string work = WorkTextBox.Text;
+        string password = PasswordTextBox.Text;
+
+        Community_AssistEntities cae = new Community_AssistEntities();
+        int result = cae.usp_Register(lastname, FirstNameTextBox, email, password, apartment, street, city, state, zip, home, work);
+
+        if (result != -1)
+            Response.Redirect("Default.aspx");
+        else
+            ResultLabel.Text = "something went really wrong";
+    }
+}

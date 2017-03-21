183c3d1aafe87d652cc480ad04020222d57d3de1
dwilso30 <dwilso30@seattlecentral.edu>
2017-03-21 14:39:33 -0700

    Add files via upload

---
+++ Assignment7/Controllers/PersonnelController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorkAssignment7.Models;
+
+namespace NorkAssignment7.Controllers
+{
+    public class PersonnelController : Controller
+    {
+        private Community_AssistEntities db = new Community_AssistEntities();
+        // GET: Personnel
+        public ActionResult Index()
+        {
+            var peeps = from p in db.People
+                        from pa in p.PersonAddresses
+                        from c in p.Contacts
+                        where c.ContactTypeKey == 1
+                        select new
+                        {
+                            p.PersonKey,
+                            p.PersonLastName,
+                            p.PersonFirstName,
+                            p.PersonEmail,
+                            pa.PersonAddressApt,
+                            pa.PersonAddressStreet,
+                            pa.PersonAddressCity,
+                            pa.PersonAddressState,
+                            pa.PersonAddressZip,
+                            c.ContactNumber
+                        };
+            List<PersonLite> peepsList = new List<PersonLite>();
+            foreach(var p in peeps)
+            {
+                PersonLite pl = new PersonLite();
+                pl.PersonKey = (int)p.PersonKey;
+                pl.LastName = p.PersonLastName;
+                pl.FirstName = p.PersonFirstName;
+                pl.Email = p.PersonEmail;
+                pl.Apartment = p.PersonAddressApt;
+                pl.Street = p.PersonAddressStreet;
+                pl.City = p.PersonAddressCity;
+                pl.State = p.PersonAddressState;
+                pl.Zipcode = p.PersonAddressZip;
+                peepsList.Add(pl);
+            }
+
+            return View(peepsList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Create([Bind(Include = "LastName, FirstName, Email, Password, Apartment, Street, City, State, Zipcode, homePhone, WorkPhone")]
+        PersonLite pl)
+        {
+            int result = db.usp_Register(pl.LastName, pl.FirstName, pl.Email, pl.Password, pl.Apartment, pl.Street, pl.City, pl.State, pl.Zipcode, pl.HomePhone, pl.WorkPhone);
+            return View();
+        }
+    }
+}
---
+++ Assignment7/README.md
@@ -0,0 +1,17 @@
+# ITC172wn17Assignment7
+Create an MVC project.
+
+It will use data entities based on Community_Assist as a model.
+The project should let you view people's first name, last name and email,  but without their password hash and seed.
+Modify the Detail and the edit so it only contains the fields in the select.
+Also modify the create so that it uses the usp_add person stored procedure.
+Add Person to the menu on the startup page. Modify the startup page to include the heading Community Assist, and a brief description of the charity.
+Resources
+I have created a three part tutorial on MVC: What you get for Free
+ (Links to an external site.)
+, Modifying the Generated Code
+ (Links to an external site.)
+, and Creating a Controller and views from Scratch
+ (Links to an external site.)
+All the code from the tutorials is at https://github.com/spconger/BookReviewMVC
+ (Links to an external site.)
